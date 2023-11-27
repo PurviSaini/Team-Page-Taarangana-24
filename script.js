99d4b8e066868c82c36a3496181a74391588db7e
Shikha Dhiman <dshikha4403@gmail.com>
2023-11-27 20:47:10 +0530

    Applied animation on the home team page

--- script.js
+++ script.js
@@ -6,64 +6,64 @@ let container;
 let camera, scene, renderer;
 let uniforms;
 
-let loader = new THREE.TextureLoader();
-let texture;
-loader.setCrossOrigin("anonymous");
-loader.load(
-'https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/noise.png',
-function do_something_with_texture(tex) {
-  texture = tex;
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.minFilter = THREE.LinearFilter;
-  init();
-  animate();
-});
+// let loader = new THREE.TextureLoader();
+// let texture;
+// loader.setCrossOrigin("anonymous");
+// loader.load(
+// 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/noise.png',
+// function do_something_with_texture(tex) {
+//   texture = tex;
+//   texture.wrapS = THREE.RepeatWrapping;
+//   texture.wrapT = THREE.RepeatWrapping;
+//   texture.minFilter = THREE.LinearFilter;
+//   init();
+//   animate();
+// });
 
 
-function init() {
-  container = document.querySelector('#container');
+// function init() {
+//   container = document.querySelector('#container');
 
-  camera = new THREE.Camera();
-  camera.position.z = 1;
+//   camera = new THREE.Camera();
+//   camera.position.z = 1;
 
-  scene = new THREE.Scene();
+//   scene = new THREE.Scene();
 
-  var geometry = new THREE.PlaneBufferGeometry(2, 2);
+//   var geometry = new THREE.PlaneBufferGeometry(2, 2);
 
-  uniforms = {
-    u_time: { type: "f", value: 1.0 },
-    u_resolution: { type: "v2", value: new THREE.Vector2() },
-    u_noise: { type: "t", value: texture },
-    u_mouse: { type: "v2", value: new THREE.Vector2() } };
+//   uniforms = {
+//     u_time: { type: "f", value: 1.0 },
+//     u_resolution: { type: "v2", value: new THREE.Vector2() },
+//     u_noise: { type: "t", value: texture },
+//     u_mouse: { type: "v2", value: new THREE.Vector2() } };
 
 
-  var material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('fragmentShader').textContent });
+//   var material = new THREE.ShaderMaterial({
+//     uniforms: uniforms,
+//     vertexShader: document.getElementById('vertexShader').textContent,
+//     fragmentShader: document.getElementById('fragmentShader').textContent });
 
-  material.extensions.derivatives = true;
+//   material.extensions.derivatives = true;
 
-  var mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+//   var mesh = new THREE.Mesh(geometry, material);
+//   scene.add(mesh);
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
+//   renderer = new THREE.WebGLRenderer();
+//   renderer.setPixelRatio(window.devicePixelRatio);
 
-  container.appendChild(renderer.domElement);
+//   container.appendChild(renderer.domElement);
 
-  onWindowResize();
-  window.addEventListener('resize', onWindowResize, true);
+//   onWindowResize();
+//   window.addEventListener('resize', onWindowResize, true);
 
-  document.addEventListener('pointermove', e => {
-    let ratio = window.innerHeight / window.innerWidth;
-    uniforms.u_mouse.value.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio;
-    uniforms.u_mouse.value.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1;
+//   document.addEventListener('pointermove', e => {
+//     let ratio = window.innerHeight / window.innerWidth;
+//     uniforms.u_mouse.value.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio;
+//     uniforms.u_mouse.value.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1;
 
-    e.preventDefault();
-  });
-}
+//     e.preventDefault();
+//   });
+// }
 
 function onWindowResize(event) {
   renderer.setSize(window.innerWidth, window.innerHeight);
