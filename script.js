74c5c34c6f22b58d04f638e2c431ee2ebf1fbe94
Shikha Dhiman <dshikha4403@gmail.com>
2023-12-27 20:03:01 +0530

    Added start button to team page

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 AOS.init({
   duration: 1000,
 });
+
+// document.getElementById("start-btn").addEventListener("mouseover", function(){
+//   // this.style.cursor = pointer;
+//   this.style.color = 'red';
+//   console.log('Hello');
+// })
+// $(document).ready(function(){
+//   $('.start-btn').click(function(){
+//     $('.start-btn').animate({
+//       fontSize:"12px"
+//     })
+//   })
+// })
+
+$('.start-btn').click(function(){
+    $(this).fadeIn();
+})
