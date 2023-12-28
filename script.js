df1f01e0146ba84847a5cd171e969b06ccb42d41
Shikha Dhiman <dshikha4403@gmail.com>
2023-12-28 23:28:12 +0530

    modified hover effect on start button

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
       $('.imagecontainer').html("")
       $('.imagecontainer').css("background-image","none");
       $('.imagecontainer').css("background-color","rgb(178, 178, 178)");
-      $('.imagecontainer').slideUp(1000);
-      $('.imagecontainer').scroll();
+      $('.imagecontainer').slideUp(1000, function(){
+        $(this).scroll();
+      });
+      // $('.imagecontainer').scroll();
       $('.circles').hide();
   })
 
