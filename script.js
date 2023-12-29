7ce041b62b8a4f8c60f0e7c51c6af85dbac4a1ed
Purvi Saini <purvitashu450@gmail.com>
2023-12-29 14:55:35 +0530

    tranistion to home page

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
   AOS.init({
     duration: 1000,
   });
+  $('.teams-container').hide();
   $('#start-btn').click(function(event){
     event.preventDefault();
     $(this).fadeOut(500)
     $('.wrapper-heading, #start-btn-wrap').fadeOut(500);
     $('.imagecontainer').slideUp(1700);
+    $('.circles').hide();
+    $('.teams-container').show();
+
   });
 
 })
