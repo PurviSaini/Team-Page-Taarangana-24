cd5c13716d064cb410f6d559bdbe5eb48ce6ff2c
Purvi Saini <purvitashu450@gmail.com>
2024-01-02 22:45:33 +0530

    formatted the document

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-$(document).ready(function(){
-
-    AOS.init({
-      duration: 1000,
-    });
+$(document).ready(function () {
+    
     $('.teams-container').hide();
-    $('#start-btn').click(function(event){
-      event.preventDefault();
-      $(this).fadeOut(500)
-      $('.wrapper-heading, #start-btn-wrap').fadeOut(500);
-      $('.imagecontainer').slideUp(1700);
-      $('.circles').hide();
-      $('.teams-container').show();
-  
+    $('#start-btn').click(function (event) {
+        event.preventDefault();
+        $(this).fadeOut(500)
+        $('.wrapper-heading, #start-btn-wrap').fadeOut(500);
+        $('.imagecontainer').slideUp(1700);
+        $('.circles').hide();
+        $('.teams-container').show();
+
     });
-  
-  })
+
+})
